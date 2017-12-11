c7ee4f6c6af759ffa83b079b81ba88a15e0dfb38
Shang-Hung Tsai <seantsai726@gmail.com>
2017-12-11 18:40:15 -0500

    fix sql close() in rate.php

--- rate.php
+++ rate.php
@@ -16,10 +16,15 @@
 	$delete_query = $conn->prepare("DELETE FROM Rate WHERE Username = ? AND TrackId = ?");
 	$delete_query->bind_param('ss', $_SESSION['Username'], $_POST['track']);
 	$delete_query->execute();
+	$delete_query->close();
     } 
     $query = $conn->prepare("INSERT INTO `Rate` VALUES (?, ?, ?, ?)");
     $query->bind_param('ssss', $_SESSION['Username'], $_POST['track'], $_POST['score'], date('Y-m-d H:i:s', time()));
     $query->execute();
+
+    $check_rate->close();
+    $query->close();
+    $conn->close();
 
     header("Location: track.php?track=" . $_POST['track']);
 ?>
